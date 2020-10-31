90fed9c35b3f49f9082c07be9526aedd6a67ae9c
tobeking247 <phattest11@gmail.com>
2020-10-31 09:18:16 +0700

    add testcase checkboxTest, dropdownTest, hyperlinkTest

---
+++ src/test/java/findElement/checkboxTest.java
@@ -0,0 +1,36 @@
+package findElement;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class checkboxTest {
+
+    @Test
+    void verifyCheckboxes(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        WebElement checkbox1 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[1]"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[2]"));
+
+        if (checkbox1.isSelected()){
+            checkbox1.click();
+        }
+
+        Assert.assertTrue(checkbox1.isSelected());
+
+        if (checkbox2.isSelected()){
+            checkbox2.click();
+        }
+        Assert.assertTrue(checkbox2.isSelected());
+        driver.quit();
+    }
+    private void check(WebElement element){
+
+    };
+}
---
+++ src/test/java/findElement/hyperlinkTest.java
@@ -0,0 +1,32 @@
+package findElement;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class hyperlinkTest {
+    @Test
+    void checkHyperlink(){
+        WebDriverManager.chromedriver().setup();
+        String baseurl = "https://the-internet.herokuapp.com/status_codes";
+        WebDriver driver = new ChromeDriver();
+        driver.get(baseurl);
+
+        driver.findElement(By.linkText("200")).click();
+        //driver.findElement(By.xpath("//a[@href='status_codes/200']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),baseurl + "/200");
+        //navigate to homepage
+        driver.findElement(By.linkText("here")).click();// driver.navigate().back();
+
+
+        driver.findElement(By.linkText("301")).click();
+        //driver.findElement(By.xpath("//a[@href='status_codes/200']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),baseurl + "/301");
+        //navigate to homepage
+        driver.findElement(By.linkText("here")).click();
+        driver.quit();
+    }
+}
